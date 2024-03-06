4b09c3d64a24c1a73b4bf6795976b39354e519f9
saqib-786 <saqib.qumbrani@gmail.com>
2024-03-06 13:43:35 +0500

    learned request handling

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-res.write('Hello World!');
-res.end()
+    if(req.url === '/'){
+        res.write('Hello World!');
+        res.end();
+    }else if(req.url === '/form'){
+        res.setHeader('Content-Type','text/html');
+        res.write('<form> <input name="userName" /> <button>SUBMIT</button> </form>');
+        res.end();
+
+    }else{
+        res.write('404 - File Not Found');
+        res.end();
+    }
+    
+
 });
 
 server.listen(3000)
