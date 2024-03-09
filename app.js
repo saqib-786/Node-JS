cdc38c9153e05212093d3b3a09ecc115313168b5
saqib-786 <saqib.qumbrani@gmail.com>
2024-03-09 09:59:40 +0500

    Learned basic Node JS code structure

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
-const http = require('http');
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
-const server = http.createServer((req,res)=>{
-    if(req.url === '/'){
-        res.write('Hello World!');
-        res.end();
-    }else if(req.url === '/form'){
-        res.setHeader('Content-Type','text/html');
-        res.write('<form> <input name="userName" /> <button>SUBMIT</button> </form>');
-        res.end();
+const filePath = path.join(process.cwd(), "Appdata.txt");
 
-    }else{
-        res.write('404 - File Not Found');
-        res.end();
-    }
-    
-
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("Hello World!");
+    res.end();
+  } else if (req.url === "/form") {
+    res.setHeader("Content-Type", "text/html");
+    res.write(
+      '<form action="/submit" method="POST"> <input name="userName" /> <button>SUBMIT</button> </form>'
+    );
+    res.end();
+  } else if (req.url === "/submit") {
+    let data = "";
+    req.on("data", (chunk) => (data += chunk)); // geting form data into data variable
+    req.on("end", () => {
+      // After getting  complete data now reading file data
+      fs.readFile(filePath, "utf8", (err,previousData) => {
+        let newData = previousData + "\n" + data;
+        fs.writeFile(filePath, newData, () => { // wirting new data to file
+          res.setHeader("Content-Type", "text/html");
+          res.write(`<h1>Data has been saved<h1>`);
+          res.end();
+        });
+      });
+    });
+  } else {
+    res.write("404 - File Not Found");
+    res.end();
+  }
 });
 
-server.listen(3000)
+server.listen(3000);
